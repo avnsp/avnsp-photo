db68bedcbee6773a1c5a5e52413704bb91ca1a6b
Magnus Hörberg <magnus.hoerberg@gmail.com>
2013-12-18 13:27:15 +0100

    add cache_control to s3 files and resample and lower image quality

--- app.rb
+++ app.rb
@@ -9,7 +9,8 @@ require 'aws-sdk'
 TH = Thumper::Base.new(publish_to: 'amqp://localhost/avnsp',
                        consume_from: 'amqp://localhost/avnsp')
 AWS.config(access_key_id: ENV.fetch("AWS_ACCESS_KEY_ID"),
-           secret_access_key: ENV.fetch("AWS_SECRET_ACCESS_KEY"))
+           secret_access_key: ENV.fetch("AWS_SECRET_ACCESS_KEY"),
+           region: 'eu-west-1')
 TH.with_channel prefetch: 5 do |ch|
   ch.subscribe 'photo.upload', 'photo.upload' do |data|
 
@@ -19,17 +20,21 @@ TH.with_channel prefetch: 5 do |ch|
      
     image = MiniMagick::Image.read(file)
     image.quality 75
+    image.resample 72
 
     objects = s3.buckets['avnsp'].objects
 
     ct = image.mime_type
-    objects[data[:path]].write(:data => image.to_blob, :content_type => ct)
+    image.write filename
+    objects[data[:path]].write(:data => image.to_blob, :content_type => ct, cache_control: 'max-age=31536000')
 
     thumb = image.dup
     thumb.resize '100'
-    objects[data[:thumb_path]].write(:data => thumb.to_blob, :content_type => ct)
+    thumb.quality 75
+    thumb.resample 72
+    objects[data[:thumb_path]].write(:data => thumb.to_blob, :content_type => ct, cache_control: 'max-age=31536000')
+    image.write filename.sub('.jpg', '.thumb.jpg')
 
-    puts "[INFO] Compressing: #{filename} saving #{(data[:size] - file_size)/1024} KB"
     ch.publish 'photo.uploaded', data
   end
 end
