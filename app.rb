5a49cb175fa3d57dc36699657e8a809892058f70
Magnus Hörberg <magnus.hoerberg@gmail.com>
2014-02-18 19:42:18 +0100

    take connection strings from env

--- app.rb
+++ app.rb
@@ -6,8 +6,8 @@ require 'base64'
 require 'mini_magick'
 require 'aws-sdk'
 
-TH = Thumper::Base.new(publish_to: 'amqp://localhost/avnsp',
-                       consume_from: 'amqp://localhost/avnsp')
+TH = Thumper::Base.new(publish_to: ENV['CLOUDAMQP_URL'] || 'amqp://localhost/avnsp',
+                       consume_from: ENV['CLOUDAMQP_URL'] || 'amqp://localhost/avnsp')
 AWS.config(access_key_id: ENV.fetch("AWS_ACCESS_KEY_ID"),
            secret_access_key: ENV.fetch("AWS_SECRET_ACCESS_KEY"),
            region: 'eu-west-1')
